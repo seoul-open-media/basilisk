c9c4f172cfbd3555b0d788459c6badf1067c10d2
random-entity <randomentity.official@gmail.com>
2024-07-30 02:29:00 +0900

    Adopt TeensyThreads, but wrap the task methods around a while loop within lambda argument to addThread; works fine with Command/Query 100Hz; interesting cute aliasing when commanding binary oscillation at 100Hz

--- examples/tests/moteus_simple/moteus_simple.ino
+++ examples/tests/moteus_simple/moteus_simple.ino
@@ -1,34 +1,39 @@
 #include <ACAN2517FD.h>
 #include <Metro.h>
 #include <Moteus.h>
+#include <TeensyThreads.h>
 #include <initializers.h>
 
 #define CANFD_BUS 1
+
+Metro query{10};
+Metro command{10};
+Metro print{100};
 
 class ServoUnit {
  public:
   ServoUnit() : servos_{{1, CANFD_BUS}, {2, CANFD_BUS}} {}
 
   template <typename ServoCommand>
-  void CommandAll(ServoCommand c) {
+  void CommandUnit(ServoCommand c) {
     for (Servo& s : servos_) {
       c(s);
     }
   }
 
   void Query() {
-    CommandAll([](Servo& s) { s.Query(); });
+    CommandUnit([](Servo& s) { s.Query(); });
   }
 
   void Command() {
     static uint16_t count;
     double target = count % 2 ? 0.0 : 0.5;
-    CommandAll([&](Servo& s) { s.Position(target); });
+    CommandUnit([&](Servo& s) { s.Position(target); });
     count++;
   }
 
-  void Printer() {
-    CommandAll([](Servo& s) { s.Print(); });
+  void Print() {
+    CommandUnit([](Servo& s) { s.Print(); });
   }
 
   Servo servos_[2];
@@ -39,23 +44,31 @@ void setup() {
   SpiInitializer.init();
   CanFdInitializer.init(CANFD_BUS);
 
-  su.CommandAll([](Servo& s) { s.Stop(); });
+  su.CommandUnit([](Servo& s) { s.Stop(); });
+
+  threads.addThread([] {
+    while (1) {
+      if (query.check()) {
+        su.Query();
+      }
+    }
+  });
+
+  threads.addThread([] {
+    while (1) {
+      if (command.check()) {
+        su.Command();
+      }
+    }
+  });
+
+  threads.addThread([] {
+    while (1) {
+      if (print.check()) {
+        su.Print();
+      }
+    }
+  });
 }
 
-Metro query{10};
-Metro command{1000};
-Metro print{100};
-
-void loop() {
-  if (query.check()) {
-    su.Query();
-  }
-
-  if (command.check()) {
-    su.Command();
-  }
-
-  if (print.check()) {
-    su.Printer();
-  }
-}
+void loop() {}
