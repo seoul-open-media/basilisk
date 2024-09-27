25805bf6523bbd18872c2a3ec47db790feb8b664
random-entity <randomentityemail@gmail.com>
2024-09-27 20:06:47 +0900

    Misc

--- src/basilisk_teensy/basilisk_teensy.ino
+++ src/basilisk_teensy/basilisk_teensy.ino
@@ -51,9 +51,9 @@ void loop() {
   static Beat nk_cr_beat{10};
   if (nk_cr_beat.Hit()) nk_cr.Run();
 
-  static Beat executer_beat{10};
-  if (executer_beat.Hit()) exec.Run();
+  static Beat exec_beat{10};
+  if (exec_beat.Hit()) exec.Run();
 
-  static Beat serial_plotter_rs_beat{250};
-  if (serial_plotter_rs_beat.Hit()) SerialReplySender(b);
+  static Beat serial_rs_beat{250};
+  if (serial_rs_beat.Hit()) SerialReplySender(b);
 }
